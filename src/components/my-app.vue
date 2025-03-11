20769d5d1d7a881286359df745f437bdf74557b8
George Wu <george.wu@rmit.edu.au>
2025-03-11 17:02:59 +1100

    update my-app.vue to enhance world click functionality by adding coordinate storage and updating the geojson structure

--- src/components/my-app.vue
+++ src/components/my-app.vue
@@ -1,7 +1,7 @@
 <template>
     <v-app>
         <v-main>
-            <v-container>
+            <v-container style="background: white; min-height: 100vh;">
                 <h1>Hello World From RMIT</h1>
                 <div>
                     <p>X: {{ x }}</p>
@@ -24,13 +24,28 @@ export default {
         return {
             x: null,
             y: null,
-            z: null
-        };
+            z: null,
+            world_on_click_coordinate: {
+                widgetID: widget.id,
+                geojson: {
+                    type: "FeatureCollection",
+                    name: "sensor-points",
+                    crs: { type: "name", properties: { name: "urn:ogc:def:crs:EPSG::3857" } },
+                    features: [
+                        {
+                            type: "Feature",
+                            properties: { "id": 1, "Temperature": 0, "Humidity": 0, "Wind Speed": 0 },
+                            geometry: { type: "Point", coordinates: [this.x, this.y, this.z] }
+                        }]
+                }
+            },
+        }
     },
     computed: {
         ...mapStores(useGlobalStore)
     },
     async mounted() {
+        console.log("App mounted");
         this.platformAPI = await requirejs("DS/PlatformAPI/PlatformAPI");
         this.platformAPI.subscribe("3DEXPERIENCity.OnWorldClick", this.handleWorldClick);
     },
@@ -40,7 +55,16 @@ export default {
             this.x = res.x;
             this.y = res.y;
             this.z = res.z;
+            
+            this.world_on_click_coordinate.geojson.features[0].geometry.coordinates = [this.x, this.y, this.z];
+            this.platformAPI.publish("3DEXPERIENCity.Add3DPOI", this.world_on_click_coordinate);
         }
     }
 };
 </script>
+
+<style>
+.v-application {
+    background: white !important;
+}
+</style>
