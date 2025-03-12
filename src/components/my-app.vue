695e3fc00d6be2eca46f80a37db1fc82ecc43c24
George Wu <george.wu@rmit.edu.au>
2025-03-12 16:33:30 +1100

    update my-app.vue to remove color change functionality and implement temperature updates for tree layer

--- src/components/my-app.vue
+++ src/components/my-app.vue
@@ -14,12 +14,6 @@
                     >
                         Create Point
                     </v-btn>
-                    <v-btn 
-                        color="secondary" 
-                        @click="changeColor"
-                    >
-                        Change Color
-                    </v-btn>
                 </div>
             </v-container>
         </v-main>
@@ -38,18 +32,18 @@ export default {
             x: null,
             y: null,
             z: null,
-            pointCreated: false,
-            colorIndex: 0,
-            colors: ['rgb(255,0,0)', 'rgb(0,0,255)', 'rgb(0,255,0)'],
+
             tree_coordinate: {
                 widgetID: widget.id,
                 geojson: {
                     type: "FeatureCollection",
                     name: "tree_coordinates",
                     crs: { type: "name", properties: { name: "urn:ogc:def:crs:EPSG::7855" } },
+
                     features: [
                         {
                             type: "Feature",
+                            properties: { "id": 0, "Temperature": 0, "Humidity": 0, "Wind Speed": 0 },
                             geometry: { type: "Point", coordinates: [344743.73853630596, 5966167.156872547, 120.72197453345325] }
                         }]
                 },
@@ -59,23 +53,31 @@ export default {
                 },
                 render: {
                     anchor: true,
-                    color: "rgb(0,255,0)",
+                    color: "blue",
                     scale: [1, 1, 1],
                     shape: "sphere",
-                    switchDistance: 500,
                     opacity: 1
                 }
             },
+            temperatureInterval: null,
         }
     },
     computed: {
         ...mapStores(useGlobalStore)
     },
+
     async mounted() {
         console.log("App mounted");
         this.platformAPI = await requirejs("DS/PlatformAPI/PlatformAPI");
         this.platformAPI.subscribe("3DEXPERIENCity.OnWorldClick", this.handleWorldClick);
+        this.startTemperatureUpdate();
     },
+    beforeUnmount() {
+        if (this.temperatureInterval) {
+            clearInterval(this.temperatureInterval);
+        }
+    },
+
     methods: {
         handleWorldClick(res) {
             console.log("World Clicked Millie Says", res);
@@ -83,30 +85,27 @@ export default {
             this.y = res.y;
             this.z = res.z;
         },
+
         createPoint() {
             console.log("Creating Point");
             this.platformAPI.publish("3DEXPERIENCity.Add3DPOISet", this.tree_coordinate);
             this.pointCreated = true;
         },
-        changeColor() {
-            this.colorIndex = (this.colorIndex + 1) % 3;
-            const currentColor = this.colors[this.colorIndex];
-            console.log("Changing color to:", currentColor);
-            
-            const updateContent = {
-                widgetID: widget.id,
-                layerID: "tree-layer",
-                geojson: {
-                    type: "FeatureCollection",
-                    features: this.tree_coordinate.geojson.features,
-                    render: {
-                        ...this.tree_coordinate.render,
-                        color: currentColor
-                    }
-                }
-            };
-            
-            this.platformAPI.publish("3DEXPERIENCity.Update3DPOIContent", updateContent);
+
+        startTemperatureUpdate() {
+            this.temperatureInterval = setInterval(() => {
+                this.tree_coordinate.geojson.features[0].properties.Temperature += 1;
+                
+                // Update POI with new temperature
+                const updateContent = {
+                    widgetID: widget.id,
+                    layerID: "tree-layer",
+                    geojson: this.tree_coordinate.geojson
+                };
+                
+                this.platformAPI.publish("3DEXPERIENCity.Update3DPOIContent", updateContent);
+                console.log("Temperature updated:", this.tree_coordinate.geojson.features[0].properties.Temperature);
+            }, 1000);
         }
     }
 };
