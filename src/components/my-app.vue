52f80c12995730e438981d01001bd70cde59d12a
George Wu <george.wu@rmit.edu.au>
2025-03-14 11:35:36 +1100

    add: implement methods to create 3D POIs and set temperature updates automatically

--- src/components/my-app.vue
+++ src/components/my-app.vue
@@ -13,7 +13,10 @@
                     <p>X: {{ x }}</p>
                     <p>Y: {{ y }}</p>
                     <p>Z: {{ z }}</p>
-                    <v-btn color="primary" class="mr-2" @click="createPoint"> Create Point </v-btn>
+                    <v-btn color="primary" class="mr-2" @click="create3DPOI"> Create Point </v-btn>
+                    <v-btn color="primary" class="mr-2" @click="create3DPOISet"> Create Point SET</v-btn>
+                    <v-btn color="primary" class="mr-2" @click="create3DPOISetPopulate"> Create Point SET Populate </v-btn>
+
                     <v-btn color="error" class="ml-2" @click="removePoint" :disabled="!pointExists"> Remove Point </v-btn>
                     <v-btn color="warning" class="ml-2" @click="setTemperature" :disabled="!pointExists"> Set Temperature to 10 </v-btn>
                     <v-btn color="info" class="ml-2" @click="updateTemperature" :disabled="!pointExists"> Update Temperature to 10 </v-btn>
@@ -79,7 +82,8 @@ export default {
             },
             pointExists: false,
             mqttClient: null,
-            currentTemperature: 0
+            currentTemperature: 0,
+            temperatureInterval: null
         };
     },
     computed: {
@@ -90,6 +94,15 @@ export default {
         console.log("App mounted");
         this.platformAPI = await requirejs("DS/PlatformAPI/PlatformAPI");
         this.platformAPI.subscribe("3DEXPERIENCity.OnWorldClick", this.handleWorldClick);
+        
+        // Start temperature update interval
+        this.temperatureInterval = setInterval(this.updateTemperatureAutomatic, 5000);
+    },
+
+    beforeUnmount() {
+        if (this.temperatureInterval) {
+            clearInterval(this.temperatureInterval);
+        }
     },
 
     methods: {
@@ -100,11 +113,31 @@ export default {
             this.z = res.z;
         },
 
-        createPoint() {
+        create3DPOI() {
             console.log("Creating Point");
             this.platformAPI.publish("3DEXPERIENCity.Add3DPOI", this.tree_coordinate);
             this.platformAPI.subscribe("3DEXPERIENCity.Add3DPOIReturn", res => {
                 console.log("MIlle Says Add3DPOIReturn", res);
+            });
+
+            this.pointExists = true;
+        },
+        
+        create3DPOISet() {
+            console.log("Creating Point");
+            this.platformAPI.publish("3DEXPERIENCity.Add3DPOISet", this.tree_coordinate);
+            this.platformAPI.subscribe("3DEXPERIENCity.Add3DPOISetReturn", res => {
+                console.log("MIlle Says Add3DPOISetReturn", res);
+            });
+
+            this.pointExists = true;
+        },
+
+        create3DPOISetPopulate() {
+            console.log("Creating Point");
+            this.platformAPI.publish("3DEXPERIENCity.Populate3DPOISet", this.tree_coordinate);
+            this.platformAPI.subscribe("3DEXPERIENCity.Populate3DPOISetReturn", res => {
+                console.log("MIlle Says Populate3DPOISetReturn", res);
             });
 
             this.pointExists = true;
@@ -154,6 +187,35 @@ export default {
             this.platformAPI.subscribe("3DEXPERIENCity.Update3DPOIContentReturn", res => {
                 console.log("MIlle Says Update3DPOIContentReturn", res);
             });
+        },
+
+        updateTemperatureAutomatic() {
+            if (!this.pointExists) return;
+            
+            this.currentTemperature += 1;
+            const updateContent = {
+                widgetID: widget.id,
+                layerID: "tree-layer",
+                geojson: {
+                    type: "FeatureCollection",
+                    name: "tree_coordinates",
+                    crs: { type: "name", properties: { name: "urn:ogc:def:crs:EPSG::7855" } },
+                    features: [
+                        {
+                            type: "Feature",
+                            properties: { "id": 0, "Temperature": this.currentTemperature, "Humidity": 10, "Wind Speed": 0 },
+                            geometry: { type: "Point", coordinates: [344743.73853630596, 5966167.156872547, 120.72197453345325] }
+                        },
+                        {
+                            type: "Feature",
+                            properties: { "id": 0, "Temperature": this.currentTemperature, "Humidity": 0, "Wind Speed": 10 },
+                            geometry: { type: "Point", coordinates: [344748.34285080613, 5966162.11668492, 120.66504647215706] }
+                        }
+                    ]
+                }
+            };
+
+            this.platformAPI.publish("3DEXPERIENCity.Update3DPOIContent", updateContent);
         }
     },
 
