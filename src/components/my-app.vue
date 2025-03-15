38e55b997ee5de92f86b79398298f23e1d12b07c
“George <George.wu@rmit.edu.au>
2025-03-15 16:12:36 +1100

    feat: enhance item selection handling and clean up unused code in my-app.vue

--- src/components/my-app.vue
+++ src/components/my-app.vue
@@ -8,7 +8,6 @@
                     <p>Y: {{ y }}</p>
                     <p>Z: {{ z }}</p>
                     <v-btn color="primary" class="mr-2" @click="create3DPOI"> Create Point </v-btn>
-                    
                     <v-btn color="error" class="ml-2" @click="removePoint" :disabled="!pointExists"> Remove Point </v-btn>
                     <v-btn color="info" class="ml-2" @click="updateTemperature" :disabled="!pointExists"> Update Temperature to 10 </v-btn>
                 </div>
@@ -18,10 +17,11 @@
 </template>
 
 <script>
-import { widget } from "@widget-lab/3ddashboard-utils";
 import { mapStores } from "pinia";
-import { useGlobalStore } from "@/store/global";
+import { widget } from "@widget-lab/3ddashboard-utils";
 import geojson from "@/assets/sundial_orchard_tree_information.geojson"; // Import the geojson file
+import { useGlobalStore } from "@/store/global";
+
 // import mqtt from "mqtt";
 
 export default {
@@ -36,10 +36,9 @@ export default {
                 widgetID: widget.id,
                 geojson: geojson, // Use the imported geojson file
                 folder: {
-                    id:"tree-folder",
-                    name: "tree folder",
+                    id: "tree-folder",
+                    name: "tree folder"
                 },
-
                 layer: {
                     id: "tree-layer",
                     name: "tree POI",
@@ -73,36 +72,8 @@ export default {
         console.log("App mounted");
         this.platformAPI = await requirejs("DS/PlatformAPI/PlatformAPI");
         this.platformAPI.subscribe("3DEXPERIENCity.OnWorldClick", this.handleWorldClick);
-        
-    //     // Connect to MQTT broker using WebSocket
-    //     const options = {
-    //         protocol: 'ws',
-    //         hostname: '54.206.8.77',
-    //         port: 8083,
-    //         path: '/mqtt',
-    //         clientId: 'widget_' + Math.random().toString(16).substr(2, 8)
-    //     };
-        
-    //     // this.mqttClient = mqtt.connect(options);
-    //     this.mqttClient = mqtt.connect('ws://54.206.8.77:8083');
-        
-    //     this.mqttClient.on('connect', () => {
-    //         console.log('Connected to MQTT broker');
-    //         this.mqttClient.subscribe('sensor/temperature');
-    //     });
-
-    //     this.mqttClient.on('error', (error) => {
-    //         console.error('MQTT connection error:', error);
-    //     });
-
-    //     this.mqttClient.on('message', (topic, message) => {
-    //         if (topic === 'sensor/temperature') {
-    //             this.currentTemperature = parseFloat(message.toString());
-    //             if (this.pointExists) {
-    //                 this.updateTemperatureAutomatic();
-    //             }
-    //         }
-    //     });
+        this.platformAPI.subscribe("3DEXPERIENCity.OnItemSelect", this.handleOnItemSelect);
+        this.platformAPI.subscribe("3DEXPERIENCity.OnItemDeselect", this.handleOnItemDeselect);
     },
 
     // beforeUnmount() {
@@ -118,16 +89,50 @@ export default {
     methods: {
         handleWorldClick(res) {
             console.log("World Clicked Millie Says", res);
-            this.x = res.x; 
+            this.x = res.x;
             this.y = res.y;
             this.z = res.z;
+        },
+
+        handleOnItemSelect(res) {
+            console.log("Item Selected Millie Says", res);
+            this.getSelectedItems(res);
+            this.getListAttributes(res);
+            this.get(res);
+        },
+
+        handleOnItemDeselect(res) {
+            console.log("Item Deselected Millie Says", res);
+            // Handle item deselection if needed
+        },
+
+        getSelectedItems(res) {
+            this.platformAPI.publish("3DEXPERIENCity.GetSelectedItems", res);
+            this.platformAPI.subscribe("3DEXPERIENCity.GetSelectedItemsReturn", res => {
+                console.log("Mille Says GetSelectedItemsReturn", res);
+            });
+        },
+
+        getListAttributes(res) {
+            this.platformAPI.publish("3DEXPERIENCity.GetListAttributes", res);
+            this.platformAPI.subscribe("3DEXPERIENCity.GetListAttributesReturn", res => {
+                console.log("Mille Says GetListAttributesReturn", res);
+            });
+        },
+
+
+        get(res) {
+            this.platformAPI.publish("3DEXPERIENCity.Get",res);
+            this.platformAPI.subscribe("3DEXPERIENCity.GetReturn", res => {
+                console.log("Mille Says GetReturn", res);
+            });
         },
 
         create3DPOI() {
             console.log("Creating Point");
             this.platformAPI.publish("3DEXPERIENCity.Add3DPOI", this.tree_coordinate);
             this.platformAPI.subscribe("3DEXPERIENCity.Add3DPOIReturn", res => {
-                console.log("MIlle Says Add3DPOIReturn", res);
+                console.log("Mille Says Add3DPOIReturn", res);
             });
 
             this.pointExists = true;
@@ -139,9 +144,7 @@ export default {
             this.pointExists = false;
         },
 
-
         updateTemperature() {
-
             const updateContent = {
                 widgetID: widget.id,
                 layerID: "tree-layer",
@@ -154,25 +157,17 @@ export default {
                             type: "Feature",
                             properties: { "id": 0, "Temperature": 10, "Humidity": 10, "Wind Speed": 0 },
                             geometry: { type: "Point", coordinates: [344743.73853630596, 5966167.156872547, 120.72197453345325] }
-                        },
-                        // {
-                        //     type: "Feature",
-                        //     properties: { "id": 0, "Temperature": 10, "Humidity": 0, "Wind Speed": 10 },
-                        //     geometry: { type: "Point", coordinates: [344748.34285080613, 5966162.11668492, 120.66504647215706] }
-                       
-                        // }
+                        }
                     ]
                 }
             };
 
             this.platformAPI.publish("3DEXPERIENCity.Update3DPOIContent", updateContent);
             this.platformAPI.subscribe("3DEXPERIENCity.Update3DPOIContentReturn", res => {
-                console.log("MIlle Says Update3DPOIContentReturn", res);
+                console.log("Mille Says Update3DPOIContentReturn", res);
             });
-        },
-
-    },
-
+        }
+    }
 };
 </script>
 
@@ -180,7 +175,6 @@ export default {
 .v-application {
     background: white !important;
 }
-
 
 .temperature-display {
     max-width: 300px;
