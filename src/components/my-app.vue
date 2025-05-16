b3fdfa0103cc33885e16589b60e6788d6afeeacc
George Wu <george.wu@rmit.edu.au>
2025-05-16 12:54:12 +1000

    feat: enhance soil data handling by adding soil moisture layers and refactoring 3D POI creation

--- src/components/my-app.vue
+++ src/components/my-app.vue
@@ -24,10 +24,69 @@ export default {
         return {
             mqttClient: null,
             soilData: null,
-            selectedItem: null,
-            layerListAttributes: null,
-            attributeValue: null,
-            updatedAttributeValue: null
+
+            soilMoistureLowGeoJSON: null,
+            soilMoistureNormalGeoJSON: null,
+
+            soilDataLayer: {
+                widgetID: widget.id,
+                geojson: soilGeoJSON,
+                layer: {
+                    id: "soil_sensor_layer",
+                    name: "soil_sensor_layer",
+                    attributeMapping: {
+                        "STRID": "GUID",
+                    }
+                },
+                render: {
+                    anchor: true,
+                    color: "blue",
+                    scale: [1, 1, 1],
+                    shape: "tube",
+                    switchDistance: 500,
+                    opacity: 0.5
+                }
+            },
+
+            soilMoistureLowLayer: {
+                widgetID: widget.id,
+                geojson: null,
+                layer: {
+                    id: "soil_moisture_low_layer",
+                    name: "soil_moisture_low_layer",
+                    attributeMapping: {
+                        "STRID": "GUID",
+                    }
+                },
+                render: {
+                    anchor: true,
+                    color: "red",
+                    scale: [1, 1, 1],
+                    shape: "tube",
+                    switchDistance: 500,
+                    opacity: 1
+                }
+            },
+
+            soilMoistureNormalLayer: {
+                widgetID: widget.id,
+                geojson: null,
+                layer: {
+                    id: "soil_moisture_normal_layer",
+                    name: "soil_moisture_normal_layer",
+                    attributeMapping: {
+                        "STRID": "GUID",
+                    }
+                },
+                render: {
+                    anchor: true,
+                    color: "green",
+                    scale: [1, 1, 1],
+                    shape: "tube",
+                    switchDistance: 500,
+                    opacity: 1
+                }
+            },
         };
     },
     computed: {
@@ -60,7 +119,7 @@ export default {
 
         this.mqttClient.on("message", (topic, message) => {
             if (topic === "sensor/soil_moisture") {
-                this.soil_data = JSON.parse(message.toString());
+                this.soilData = JSON.parse(message.toString());
             }
         });
     },
@@ -73,71 +132,9 @@ export default {
 
     methods: {
         createSoilSensor3DPOI() {
-            const soil_data_layer = {
-                widgetID: widget.id,
-                geojson: soilGeoJSON,
-                layer: {
-                    id: "soil_sensor_layer",
-                    name: "soil_sensor_layer",
-                    attributeMapping: {
-                        "STRID": "GUID",
-                    }
-                },
-                render: {
-                    anchor: true,
-                    color: "blue",
-                    scale: [1, 1, 1],
-                    shape: "tube",
-                    switchDistance: 500,
-                    opacity: 0.5
-                }
-            };
-            this.platformAPI.publish("3DEXPERIENCity.Add3DPOI", soil_data_layer);
+            this.platformAPI.publish("3DEXPERIENCity.Add3DPOI", this.soilDataLayer);
         }
 
-        // handleOnItemSelect(res) {
-        //     this.platformAPI.publish("3DEXPERIENCity.GetSelectedItems", res);
-        //     this.platformAPI.subscribe("3DEXPERIENCity.GetSelectedItemsReturn", res => {
-        //         if (res.data && res.data.length > 0) {
-        //             const item = res.data[0];
-        //             this.selectedItem = {
-        //                 id: item.id,
-        //                 name: item.name,
-        //                 className: item.className,
-        //                 position: item.position,
-        //                 visible: item.visible,
-        //                 userData: item.userData
-        //             };
-        //         } else {
-        //             this.selectedItem = null;
-        //         }
-        //     });
-
-        //     this.platformAPI.publish("3DEXPERIENCity.GetListAttributes", res);
-        //     this.platformAPI.subscribe("3DEXPERIENCity.GetListAttributesReturn", res => {
-        //         if (res && Array.isArray(res)) {
-        //             this.layerListAttributes = res;
-        //         }
-        //     });
-
-        //     const attribute = "userData";
-        //     this.platformAPI.publish("3DEXPERIENCity.Get", [this.selectedItem.id, attribute]);
-        //     this.platformAPI.subscribe("3DEXPERIENCity.GetReturn", res => {
-        //         console.log("Get Return: ", res);
-        //         if (res) {
-        //             this.attributeValue = res;
-        //             this.updatedAttributeValue = JSON.parse(JSON.stringify(this.attributeValue));
-        //             // this.updatedAttributeValue.features[0].properties.fruit_type = "Kiwi";
-        //             this.updatedAttributeValue.fruit_type = "Kiwi";
-        //         }
-        //     });
-
-        // },
-        // updateGeoJSONFruitTypeBanana() {
-        //     // this.updatedAttributeValue.features[0].properties.fruit_type = "Banana";
-        //     this.updatedAttributeValue.fruit_type = "Banana";
-        //     this.platformAPI.publish("3DEXPERIENCity.Set", [this.selectedItem.id, "geojson", this.updatedAttributeValue]);
-        // }
     }
 };
 </script>
